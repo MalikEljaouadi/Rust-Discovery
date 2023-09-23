efc2a8a8dbb0ed659c4a4b4f83ad3416480e16f0
Malik Eljaouadi <malik.eljaouadi@wefox.com>
2023-09-23 15:44:07 +0200

    🔨 Add a loop and break in case of win

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,27 +1,33 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("this is the Guess number game!");
 
-    println!("input the number");
-    
-    let secret_number = rand::thread_rng().gen_range(1..101);
-    
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Error while reading input number");
-    
-    let guess:u32 = guess.trim().parse().expect("Unable to parse");
+    loop {
+        println!("input the number");
+        let secret_number = rand::thread_rng().gen_range(1..101);
+        
+        println!("The secret_number is: {secret_number}");
+        
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error while reading input number");
 
-    println!("You guessed: {guess}");
-    
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("too small"),
-        Ordering::Equal => println!("equal"),
-        Ordering::Greater => println!("too big"),
-    }  
-    println!("The secret_number is: {secret_number}");
+        let guess: u32 = guess.trim().parse().expect("Unable to parse");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+            Ordering::Greater => println!("too big"),
+        }
+        
+    }
 }
